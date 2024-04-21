d0e46a315069cc6acae226775c4d8eb8329cc300
Joydeep Sarkar <138491246+joydeepsarkar99@users.noreply.github.com>
2024-04-21 19:25:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 // Complete the js code
 function Car(make, model) {
-	constructor(make,model){
-		this.make = make
-		this.model = model
-	}
-	getMakeModel(){
-		return `${this.make} ${this.model}`
-	}
+	this.make = make;
+    this.model = model;
+}
+Car.prototype.getMakeModel = function(){
+	return `${this.make} ${this.model}`
 }
 
-function SportsCar extends Car(make, model, topSpeed) {
-	constructor(make,model,topSpeed){
-		super(make,model)
-		this.topSpeed = topSpeed
-	}
-	getTopSpeed(){
-		return this.topSpeed
-	}
+function SportsCar(make, model, topSpeed) {
+	Car.call(this, make, model);
+	this.topSpeed = topSpeed
 }
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.getTopSpeed = function(){
+	return this.topSpeed
+}
+
+const car = new SportsCar("Ferrari", "Testarossa", 200);
+console.log(car.getMakeModel()); // Output: Ferrari Testarossa
+console.log(car.getTopSpeed()); // Output: 200
 
 // Do not change the code below
 window.Car = Car;
